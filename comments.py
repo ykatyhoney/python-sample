5290978ddfb8a2752f1a0b40399bab9ded8eb881
ykaty honey <61254756+ykatyhoney@users.noreply.github.com>
2020-11-17 06:42:17 -0800

    comments

---
+++ comments.py
@@ -0,0 +1,13 @@
+# This is a comment
+print ("Hello. Worlds!")
+
+print("Hello. Worlds") # This is a comment
+
+#print ("hello worlds")
+print("Cheers, mate!")
+
+# This is a comment
+# written in
+# more than just one line
+print("Hello, worlds")
+
